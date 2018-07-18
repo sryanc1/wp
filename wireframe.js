13925640722272722e885b8c2ed985338b3e3720
Craig Ryan <s3555490@student.rmit.edu.au>
2018-07-18 15:08:17 +0930

    Added more style and content

--- wireframe.js
+++ wireframe.js
@@ -1,4 +1,8 @@
 function toggleWireframe() {
   let wfs = document.getElementById("wireframecss");
+  let sts = document.getElementById("stylecss");
+  
   wfs.disabled = !wfs.disabled;
+  sts.disabled = sts.disabled;
+  
 }
