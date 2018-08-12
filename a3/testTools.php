047405dff3d21f89a6dfd45e1b7dbac5c0d06273
Craig Ryan <s3555490@student.rmit.edu.au>
2018-08-12 13:50:17 +0930

    trying to access details from file - see test.php

--- a3/testTools.php
+++ a3/testTools.php
@@ -1,42 +1,5 @@
+
 <?php
-	function topModule($pageTitle) {
-		$output = <<<"HEADER"
-		
-<!DOCTYPE html>
-<html lang='en'>
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>$pageTitle</title>
-		<link rel="Posie Icon" href="../../media/favicon.ico" type="image/x-icon">
-		<!-- Keep wireframe.css for debugging, add your css to style.css -->
-		<link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled>
-		<link id='stylecss' type="text/css" rel="stylesheet" href="css/style.css">
-		<script src='../wireframe.js'></script>
-		<link href="https://fonts.googleapis.com/css?family=Indie+Flower|Playfair+Display" rel="stylesheet">
-		<!-- All immages used are property of "Posie and Me" and are not for reuse -->
-	</head>
-
-	<body>
-		<div class="wrapper">
-			<header>
-				<div class="img1"><img src="../../media/PosieAndMe.png" alt="Posie and me logo" width= 200px img></div>
-				<div class="header">Posie & Me</div>
-				<h1 class="heading3">Custom made accessories for mums and bubs</h1>
-			</header>
-
-			<nav class="nav">
-				<ul>
-					<li><a href="index.php">Home</a></li>
-					<li><a href="products.php">All Products</a></li>
-					<li><a href="design.php">Design Studio</a></li>
-					<li><a href="login.php">Login</a></li>
-				</ul>  
-			</nav>
-HEADER;
-	echo $output;
-	}
-	
 	function endModule() {
 		$output = <<<"FOOTER"
 		
@@ -48,15 +11,17 @@ HEADER;
 			<div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
 			<div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
 		</footer>
-    
-		</div>
-	</body>
-</html>
 FOOTER;
 	echo $output;
 	}
+?>
 
 
+<?php
+/*Function to display all products on one page
+ - this function reads from a .txt file and outputs to HTML 
+ Addoped from w3 schools example and pumps.php example. 
+*/
 
 	function productList() {
 		
@@ -65,23 +30,30 @@ FOOTER;
 		$productArray=[]; //A 2D array of products (a table)
 		$lineHeading=[]; //First line / haeding of the table file
 		$lineArray=[]; //Contents of the table
-		
+
 		//Get the required heading details - these will become the keys of the array.
 		$lineHeading = fgetcsv($myFile, 0, "\t"); //fgetcsv() - returns an array.
 		//Loop through until the end of the file.
 		while (!feof($myFile)) {
 			//Read each line of the file - remember fgetcsv() returns a 1D array of values.
 			$lineArray = fgetcsv($myFile, 0, "\t");
+			echo "$lineArray[0]\n";
 			//For each of these values - assign a key from the "heading line" to the value.
 			foreach ($lineArray as $key => $value) {
+				echo "Key: $key and Value: $value \n";
 				//As each new line is also an array - assign a key to the array (in this case index zero). 
 				//
-				$productArray[ $lineArray[0] ][ $lineHeading[$key] ] = $lineArray[$key];	
-			}
-			
+				$productArray[ $lineArray[0]] [$lineHeading[$key]]  = $lineArray[$key];
+			}	
 		}	
 		print_r($productArray);
-		echo $productArray['001']['OID'];
+		$length1 = count($productArray); //5
+		$length2 = count($lineArray); //1
+		$length3 = count($lineHeading); //7
+		print_r($length1);
+		print_r($length2);
+		print_r($length3);
+		echo $productArray['003']['ID'];
 		flock($myFile, LOCK_UN);
 		fclose($myFile);		
 				
