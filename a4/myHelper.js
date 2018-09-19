dbf753f089d07a33e29164dba56bf5bf071a8996
Craig Ryan <s3555490@student.rmit.edu.au>
2018-09-19 11:49:33 +0930

    added coloured cells

--- a4/myHelper.js
+++ a4/myHelper.js
@@ -2,16 +2,18 @@ document.addEventListener('keypress', updateCount);
 
 function updateCount() {
 	
-	var cellArray = ["z","x","c","v","b","n","m","s","d","f","g"];
+	var cellArray = ["z","x","c","v","b","n","m","d","f","g","h"];
 	var keyName = event.key;
 	if (cellArray.includes(keyName)) {
 		var countTotal = document.getElementById("diffTotal").value;
+		countTotal = Number(countTotal);
 		if (countTotal != 100) {
 			var cellCount = document.getElementById(keyName).value;
 			cellCount = Number(cellCount);
+			document.getElementById(keyName).style.backgroundColor = "lightpink";
 			document.getElementById(keyName).value = cellCount +1;	
-			countTotal = Number(countTotal);
 			document.getElementById("diffTotal").value = countTotal +1;
+			document.getElementById("diffTotal").style.backgroundColor = "lightblue";
 		} else {
 			var audio = new Audio('audio_file.mp3');
 			audio.play();
